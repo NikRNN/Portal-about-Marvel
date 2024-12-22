914697b8eb7114d7b671334618675df8971db44c
Nikita_Romanov <romanovnnikita@gmail.com>
2024-12-22 23:34:52 +0300

    Добавлена загрузка всех персонажейи отдельного персонажа

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,19 +1,35 @@
 class MarvelService {
-   getResource = async (url) => {
-        let res = await fetch(url);
-        if(!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-        }
-        return await res.json()
+  getResource = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
+    return await res.json();
+  };
 
-    getAllCharacters = () => {
-       return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=200&apikey=63ecc63e1c62910f53b2061e0aa2656e')
-    }
+  getAllCharacters = async () => {
+    const res = this.getResource(
+      "https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=200&apikey=63ecc63e1c62910f53b2061e0aa2656e"
+    );
+    return res.data.results.map((item) => this._returnDataCharacter(item));
+  };
 
-    getSingleCharacter = (id) => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=63ecc63e1c62910f53b2061e0aa2656e`)
-   }
+  getSingleCharacter = async (id) => {
+    const res = await this.getResource(
+      `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=63ecc63e1c62910f53b2061e0aa2656e`
+    );
+    return this._returnDataCharacter(res);
+  };
+
+  _returnDataCharacter = (res) => {
+    return {
+      name: res.data.results[0].name,
+      description: res.data.results[0].description,
+      thumbnail: `${res.data.results[0].thumbnail.path}.${res.data.results[0].thumbnail.extension}`,
+      homepage: res.data.results[0].urls[0].url,
+      wiki: res.data.results[0].urls[1].url,
+    };
+  };
 }
 
 export default MarvelService;
